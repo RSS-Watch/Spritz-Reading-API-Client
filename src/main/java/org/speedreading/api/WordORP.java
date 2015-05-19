407698b1ffdc390b0437893a79dca84833bf8d65
TrickyAndye <andrejschaefer@googlemail.com>
2015-05-19 15:14:51 +0200

    Added new class WordORP for structure with duplicate entrys.

---
+++ src/main/java/org/speedreading/api/WordORP.java
@@ -0,0 +1,25 @@
+package org.speedreading.api;
+
+/**
+ * Created by Andrej on 19.05.2015.
+ */
+public class WordORP {
+    private String wordrd;
+    private Integer orp;
+
+    public String getWordrd() {
+        return wordrd;
+    }
+
+    public void setWordrd(String pword) {
+        this.wordrd = pword;
+    }
+
+    public Integer getOrp() {
+        return orp;
+    }
+
+    public void setOrp(Integer porp) {
+        this.orp = porp;
+    }
+}
